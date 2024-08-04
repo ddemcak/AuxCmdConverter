ee07b4dd7e411df8f0de4dd1ddf30d7d23b4e41a
David Demcak <ddemcak@gmail.com>
2024-08-04 16:14:35 +0200

    Title fixed. App cahnged to unpackaged.

--- AuxCmdConverter/MainWindow.xaml.cs
+++ AuxCmdConverter/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.UI.Xaml;
 using System;
+using System.ComponentModel;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Devices.Usb;
@@ -12,38 +13,59 @@ namespace AuxCmdConverter
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : Window, INotifyPropertyChanged
     {
 
         //public string CustomTitle = "Test";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+        }
 
         private string _customTitle;
         public string CustomTitle
         {
             get => _customTitle;
+            set { _customTitle = value; NotifyPropertyChanged(nameof(CustomTitle)); }
         }
 
         public MainWindow()
         {
-            _customTitle = "Ahoooooooooooooooj";
-            
             this.InitializeComponent();
-            
+            MainGrid.DataContext = this;
+
 
             this.ExtendsContentIntoTitleBar = true;
             this.SetTitleBar(AppTitleBar);
 
             this.AppWindow.MoveAndResize(new Windows.Graphics.RectInt32(150, 150, 660, 650));
-
+            /*
             this.Title = string.Format("AUX Cmd Converter v{0}.{1}.{2}.{3}",
                     Package.Current.Id.Version.Major,
                     Package.Current.Id.Version.Minor,
                     Package.Current.Id.Version.Build,
                     Package.Current.Id.Version.Revision);
+            
+            CustomTitle = string.Format("AUX Cmd Converter v{0}.{1}.{2}.{3}",
+                    Package.Current.Id.Version.Major,
+                    Package.Current.Id.Version.Minor,
+                    Package.Current.Id.Version.Build,
+                    Package.Current.Id.Version.Revision);
+            */
 
+            CustomTitle = string.Format("AUX Cmd Converter v{0}", GetAssemblyVersion());
 
             lvCommands.Items.Add("#80");
             lvCommands.Items.Add("#22,200,100");
+        }
+
+
+        private string GetAssemblyVersion()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? String.Empty;
         }
 
 
@@ -74,18 +96,24 @@ namespace AuxCmdConverter
             package.SetText(tbRawCommand.Text);
             Clipboard.SetContent(package);
 
-            lvCommands.Items.Add(tbCommand.Text);
+            lvCommands.Items.Insert(0, tbCommand.Text);
+            //lvCommands.ScrollIntoView(lvCommands.Items[lvCommands.Items.Count - 1]); 
 
         }
 
         private void lvCommands_SelectionChanged(object sender, Microsoft.UI.Xaml.Controls.SelectionChangedEventArgs e)
         {
-            tbCommand.Text = lvCommands.SelectedItem.ToString();
-            tbRawCommand.Text = BuildUpCommand(tbDevice.Text, tbCommand.Text);
+            
+            if (lvCommands.Items.Count > 0)
+            {
+                tbCommand.Text = lvCommands.SelectedItem.ToString();
+                tbRawCommand.Text = BuildUpCommand(tbDevice.Text, tbCommand.Text);
+            
+                var package = new DataPackage();
+                package.SetText(tbRawCommand.Text);
+                Clipboard.SetContent(package);
+            }
 
-            var package = new DataPackage();
-            package.SetText(tbRawCommand.Text);
-            Clipboard.SetContent(package);
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
